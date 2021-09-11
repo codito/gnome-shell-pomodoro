cd05ef286df7219799a6ac220b6431a2ca08976f
Kamil Prusko <kamilprusko@gmail.com>
2021-09-11 11:07:29 +0200

    Minor build improvements

--- meson-post-install.sh
+++ meson-post-install.sh
@@ -6,11 +6,11 @@ datadir="${prefix}/$1"
 # Distro packagers define DESTDIR and they don't
 # want/need us to do the below
 if [ -z "${DESTDIR}" ]; then
-    echo "Compiling GSchema..."
+    echo "Compiling GSettings schemas..."
     glib-compile-schemas "${datadir}/glib-2.0/schemas"
 
     echo "Updating icon cache..."
-    gtk-update-icon-cache -f -t "${datadir}/icons/hicolor"
+    gtk-update-icon-cache -qtf "${datadir}/icons/hicolor"
 
     echo "Updating desktop database..."
     update-desktop-database -q "${datadir}/applications"
